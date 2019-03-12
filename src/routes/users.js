db66934e258afaf5a6d857002daee6a6f3e40de0
Brandon <brandoncrichie@gmail.com>
2019-03-12 14:26:03 -0700

    profile bad request fix

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,11 +5,10 @@ const validation = require("./validation");
 const User = require("../../src/db/models").User;
 
 router.get("/users/signup", userController.signUp);
-router.post("/users/signup", validation.validateUsers, userController.create);
-
+router.post("/users", validation.validateUsers, userController.create);
 router.get("/users/signin", userController.signInForm);
 router.post("/users/signin", validation.validateUsers, userController.signIn);
-router.post("/users/profile", userController.profile);
+
 
 
 
