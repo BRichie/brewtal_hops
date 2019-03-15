8cbb1b745ee24e165b5c0ee05f2a24e845adf37f
Brandon <brandoncrichie@gmail.com>
2019-03-15 16:07:30 -0700

    vote bug with comment

--- src/db/models/recipe.js
+++ src/db/models/recipe.js
@@ -24,13 +24,23 @@ module.exports = (sequelize, DataTypes) => {
     }
   }, {});
   Recipe.associate = function (models) {
-    
+
+    Recipe.hasMany(models.Vote, {
+      foreignKey: "recipeId",
+      as: "votes"
+    });
+    Recipe.hasMany(models.Comment, {
+      foreignKey: "recipeId",
+      as: "comments"
+    });
+
+
     Recipe.belongsTo(models.User, {
       foreignKey: "userId",
       onDelete: "CASCADE"
     })
   };
-  Recipe.prototype.isOwner=function(){
+  Recipe.prototype.isOwner = function () {
     return this.userId === this.foreignKey;
   };
   return Recipe;
--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
-    
+
     name: {
       allowNull: false,
       type: DataTypes.STRING
@@ -11,9 +11,11 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isEmail: { msg: "must be a valid email" }
+        isEmail: {
+          msg: "must be a valid email"
+        }
       }
-    },    
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false
@@ -23,21 +25,25 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       defaultValue: "standard"
     }
-     }, {});
-  
-  
-    User.associate = function(models) {
-      User.hasMany(models.Recipe, {
-        foreignKey: "userId",
-        as: "recipes"
-      });
+  }, {});
 
-      User.prototype.isAdmin = function() {
-        return this.role === "admin";
-      };
-      User.prototype.isStandard = function () {
-        return this.role === "standard";
-      };
+
+  User.associate = function (models) {
+    User.hasMany(models.Recipe, {
+      foreignKey: "userId",
+      as: "recipes"
+    });
+    User.hasMany(models.Vote, {
+      foreignKey: "userId",
+      as: "votes"
+    });
+
+    User.prototype.isAdmin = function () {
+      return this.role === "admin";
+    };
+    User.prototype.isStandard = function () {
+      return this.role === "standard";
+    };
   };
   return User;
 };
